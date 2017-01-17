ca61e7ad463ba9765b7859e06b67ee77b41f90df
dninja777 <duroneppsjr@hotmail.com>
2017-01-17 18:40:15 +0000

    change route for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
   
   resources :docs
+  authenticated :user do
+    root "docs#index", as: "authenticated_root"
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
